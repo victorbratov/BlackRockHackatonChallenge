2cda8259c19b8db9f89213a181680764d9a27304
Will Banks <williamjb555@gmail.com>
2024-03-09 22:06:09 +0000

    Changed /help to /info, changed /show_commands to /help & modified it (WIP), /info now lists available pages

--- messageProcessing.py
+++ messageProcessing.py
@@ -1,3 +1,4 @@
+from os import listdir, path
 from types import FunctionType
 import utils
 
@@ -10,8 +11,7 @@ def requires_message(func):
     return wrapper
 
 
-
-def about() -> str:
+def about_command() -> str:
     return """
     This was a bot created to help you with financial education and budgeting.
     You can use it to track your expenses and income, and it will give you a report of your financial situation.
@@ -19,15 +19,19 @@ def about() -> str:
     """
 
 
-def help_command(message: str) -> str:
-    args = message.split(" ")
+@requires_message
+def info_command(message: str) -> str:
+    args = message.split()
     if len(args) < 2:
-        initial = """
-        Welcome to the help pages, each of which provides information on a particular aspect of personal finance.
-        To specify a help page, type "/help <page>" (replacing <page> with the page you want) and I will display it for you.
+        response = """
+        Welcome to the info pages, each of which provides information on a particular aspect of personal finance.
+        To specify an info page, type "/info <page>" (replacing <page> with the page you want) and I will display it for you.
         The pages available are:
         """
-        return initial
+        pages = [f for f in listdir("pages") if path.isfile(path.join("pages", f))]
+        for page in pages:
+            response += page[:-4] + "\n"
+        return response
     else:
         help_page_maybe = utils.get_help_page(args[1])
         if help_page_maybe is None:
@@ -37,11 +41,26 @@ def help_command(message: str) -> str:
             return help_page_maybe
 
 
-def ping(message: str) -> str:
+def ping_command() -> str:
     return 'Pong!'
 
-def show_commands() -> str:
-    return '/help\n/about\n/ping\n/show_commands'
+
+@requires_message
+def help_command(message: str) -> str:
+    args = message.split()
+    if len(args) < 2:
+        response = """
+        Available commands:
+        /ping
+        /about
+        /help
+        /info <page>
+        """
+        return response
+    else:
+        help_cmd = args[1]
+        # TODO: Return information about each command - Move the /info help here?
+        return f"You have requested help for command {help_cmd}"
 
 
 def message_is_command(message: str) -> bool:
@@ -50,10 +69,10 @@ def message_is_command(message: str) -> bool:
 
 def process_command(message: str) -> str:
     answer = {
-        '/help': help_command,
-        '/about': about,
-        '/ping': ping,
-        '/show_commands': show_commands
+        '/info': info_command,
+        '/about': about_command,
+        '/ping': ping_command,
+        '/help': help_command
     }
 
     ans = answer.get(message, 'Invalid command')
